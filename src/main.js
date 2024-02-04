4ff37f88b5a45e4da2d22a87a4daab3a39d385c3
sungsu9022 <sungsu9022@naver.com>
2024-02-05 00:01:57 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import VScrollLock from 'v-scroll-lock'
 import App from './App.vue'
+import Divider from './components/Divider.vue'
+import Button from './components/Button.vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import VueClipboard from 'vue-clipboard2'
 
-createApp(App).mount('#app')
+new AOS.init()
+
+const vueApp = createApp(App)
+
+vueApp.mount('#app')
+vueApp.use(VScrollLock)
+vueApp.use(VueClipboard)
+vueApp.use(Divider)
+vueApp.use(Button)
+vueApp.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // publicPath: '/wedding/',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/styles/_var.scss";`,
+      },
+    },
+  },
+};
