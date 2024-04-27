a04250368cded64422ae222565837c6361206176
sungsu9022 <sungsu9022@naver.com>
2024-04-27 22:25:45 +0900

    image 변경

--- src/common/viewerImages.js
+++ src/common/viewerImages.js
@@ -12,6 +12,19 @@ export default {
 		"my-wedding/gallery/9.jpg",
 		"my-wedding/gallery/10.jpg",
 		"my-wedding/gallery/11.jpg",
+		"my-wedding/gallery/12.jpg",
+		"my-wedding/gallery/13.jpg",
+		"my-wedding/gallery/14.jpg",
+		"my-wedding/gallery/15.jpg",
+		"my-wedding/gallery/16.jpg",
+		"my-wedding/gallery/17.jpg",
+		"my-wedding/gallery/18.jpg",
+		"my-wedding/gallery/19.jpg",
+		"my-wedding/gallery/20.jpg",
+		"my-wedding/gallery/21.jpg",
+		"my-wedding/gallery/22.jpg",
+		"my-wedding/gallery/23.jpg",
+		"my-wedding/gallery/24.jpg",
 	]
 
 }
