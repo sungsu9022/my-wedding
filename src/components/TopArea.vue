7c504276697ed02055487a7e9de558dc52f77499
sungsu9022 <sungsu9022@naver.com>
2024-04-29 23:48:09 +0900

    topArea 수정

--- src/components/TopArea.vue
+++ src/components/TopArea.vue
@@ -78,9 +78,8 @@ export default {
 .mcard-intro .title .dates>span{display:inline-block;padding:0 8px .1rem;border-right-width:1px;border-right-style:solid;font-size:30px;line-height:20px;text-align:center}
 .mcard-intro .title .dates>span:last-child{border:0}
 .mcard-intro .wod{letter-spacing:2px;font-size:16px}
-.mcard-intro .photo-wrap{padding:0 24px}
 .mcard-intro .photo{line-height:0}
-.mcard-intro .photo img{width:100%; margin-left: 5px; margin-right: 5px;}
+.mcard-intro .photo img{width:100%;}
 .mcard-intro .photo.border_00{border:1.2rem solid #fff;margin:0 .3rem;box-shadow:1px 2px 8px 0 rgba(54,54,54,.15)}
 .mcard-intro .bottom{margin-top:30px;font-size:.9rem}
 .mcard-intro .name{display:flex;align-items:center;justify-content:center;gap:16px;font-size:0;margin-bottom:12px;letter-spacing:1px;color:#000}
